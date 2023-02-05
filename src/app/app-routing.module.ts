27f7bbf927e323a8965928f5cded31ae837363c2
pagadala1986 <pagadala1986@gmail.com>
2023-02-05 15:43:59 +0530

    sharing the files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'services' , component:ServicesComponent},
+  {path:'prodcuts' , component:ProductsComponent},
+  {path:'clients' , component:ClientsComponent},
+  {path:'contact' , component:ContactComponent}
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeServicesComponent } from './home-services/home-services.component';
+import { ServicesComponent } from './services/services.component';
+import { ProductsComponent } from './products/products.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    FooterComponent,
+    HomeServicesComponent,
+    ServicesComponent,
+    ProductsComponent,
+    ClientsComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
